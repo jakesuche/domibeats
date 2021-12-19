2273d5b4397869198539c32c466624fa0383d74e
jakesuche <uchechidi9@gmail.com>
2021-12-19 04:10:20 +0100

    xjwx

--- src/pages/academy/academy.vue
+++ src/pages/academy/academy.vue
@@ -5,12 +5,16 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="breadcrumb__text">
-              <h2>Academy</h2>
+              <h2>Register now for september 2022</h2>
               <div class="breadcrumb__links">
-                <router-link to="/"
+                <!-- <router-link to="/"
                   ><i class="fas fa-home"></i> Home
-                </router-link>
-                <span>Academy</span>
+                </router-link> -->
+                <span
+                  >Our goal in intellectual property academy is to teach our
+                  student how to explore the world of sound and help transform
+                  their passion for music and audio into a dream career</span
+                >
               </div>
             </div>
           </div>
@@ -18,14 +22,31 @@
       </div>
     </Hero>
 
-    <section class="about spad">
+    <section class="about spad" style="margin-left: 12px;">
       <div class="container">
         <div class="d-flex justify-content-start">
-          <h2 style="color:var(--humber-light)">PROFESSIONAL COURSES</h2>
+          <h2 style="color:var(--humber-light); font-size:20px">
+            Producer Academy
+          </h2>
         </div>
-        <div class="row mt-5 mb-5">
-
-          <div v-for="post in posts" :key="post.id" class="col-md-6  mb-5 ">
+        <div class="row mt-3 mb-5">
+          <div class="col-md-12 text-light" :style="{ fontWeight: '300' }">
+            If your goal is to be a pro audio engineer, master the recording
+            process as an artist or producer, Intellectual proparty Academy
+            provides the knowledge and experience to make it a reality. Our
+            objective is to teach our students how to explore the world of sound
+            and transform their passion for music and audio into a dream career
+            while in the process, create music producers & sound engineers who
+            are well prepared for the competitive industry challenges that lie
+            ahead. IP(intellecual proparty) has a fully functional recording
+            studio and this gives us an edge in being able to expose students to
+            real-world view of production and engineering session life
+            Validation starts at Producer Academy. The Intellectual proparty
+            Producer Academy series focuses on the skills and knowledge every
+            producer needs to develop their musical skill, to maximize their
+            potential and start generating revenue for themselves.
+          </div>
+          <!-- <div v-for="post in posts" :key="post.id" class="col-md-6  mb-5 ">
             <router-link :to="`/academy/overview/${post.id}`" class="card" style="width: 80%">
               <div class="card-body academy-box">
                 <div class="academy">
@@ -34,38 +55,46 @@
                 </div>
               </div>
             </router-link>
+          </div> -->
+        </div>
+        <div class="d-flex justify-content-start">
+          <h2 style="color:var(--humber-light); font-size:17px">
+            WHAT WE OFFER
+          </h2>
+        </div>
+        <div class="row mt-3 mb-5">
+          <div class="col-md-12 text-light" :style="{ fontWeight: '300' }">
+            <span :style="{ fontWeight: '400' }">
+              Digital music production ( sound designing, sample making,
+              Arrangements):</span
+            ><br />
+
+            This course is intensive practical and theoretical to prepare
+            students to become working success stories in the commercial sound
+            industry. Students taking this course have the opportunity to learn
+            both the technical and creative components of Music Production.
           </div>
-           <!-- <div class="col-md-6 academy-box mb-5 ">
-            <router-link to="/academy/overview" class="card" style="width: 80%">
-              <div class="card-body">
-                <div class="academy">
-                  <h5>Music Production & Vocal performance</h5>
-                  <p>9 Months</p>
-                </div>
-              </div>
-            </router-link>
-          </div> -->
-           <!-- <div class="col-md-6 academy-box mb-5 ">
-            <router-link to="/academy/overview" class="card" style="width: 80%">
-              <div class="card-body">
-                <div class="academy">
-                  <h5>Music Production & Vocal performance</h5>
-                  <p>9 Months</p>
-                </div>
-              </div>
-            </router-link>
-          </div> -->
-         
-           <!-- <div class="col-md-6 academy-box mb-5 ">
-            <router-link to="/academy/overview" class="card" style="width: 80%">
-              <div class="card-body">
-                <div class="academy">
-                  <h5>Music Production & Vocal performance</h5>
-                  <p>9 Months</p>
-                </div>
-              </div>
-            </router-link>
-          </div> -->
+          <div class="col-md-12 text-light mt-3" :style="{ fontWeight: '300' }">
+            <span :style="{ fontWeight: '400' }"> Recording:</span><br />
+
+            The students will learn how to properly direct and record themselves
+            or a third party
+          </div>
+          <div class="col-md-12 text-light mt-3" :style="{ fontWeight: '300' }">
+            <span :style="{ fontWeight: '400' }"> Sound engineering:</span
+            ><br />
+
+            This course is focused on mixing and Mastering of vocals
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-center mt-3 mb-3">
+          <h2 style="color:white">Course Curriculum</h2>
+        </div>
+        <div class="d-flex justify-content-center">
+          
+              <PricingTable/>
+            
         </div>
       </div>
     </section>
@@ -77,9 +106,10 @@ import { episode_items } from "@/components/datas/card_data.js";
 import Beats from "@/components/beats/beats";
 import Hero from "@/components/hero/hero.vue";
 import image from "@/assets/img/hero/hero-video1.png";
-import { mapState } from 'vuex'
+import PricingTable from './pricingTable.vue'
+import { mapState } from "vuex";
 export default {
-  components: { Hero, Beats },
+  components: { Hero, Beats, PricingTable },
   data() {
     return {
       loading: true,
@@ -92,18 +122,16 @@ export default {
       this.loading = false;
     }, 2000);
 
-    this.$store.dispatch('audios/getPost')
+    this.$store.dispatch("audios/getPost");
   },
-  computed:{
+  computed: {
     ...mapState({
-      posts:state=>state.audios.posts
-    })
+      posts: (state) => state.audios.posts,
+    }),
   },
-  methods:{
-    getAllPost(){
-      
-    }
-  }
+  methods: {
+    getAllPost() {},
+  },
 };
 </script>
 
@@ -116,6 +144,20 @@ export default {
   border: 3px solid green;
 }
 
+.breadcrumb__links span {
+  font-size: 18px;
+  color: #f8f9fa;
+  display: inline-block;
+  width: 89%;
+}
+@media only screen and (max-width: 767px) {
+  .hero .container {
+    padding: 0 15px;
+    margin-top: -73px;
+    margin-bottom: 22px;
+  }
+}
+
 .academy-box {
   border-left: 2px solid var(--humber-golden);
 }
@@ -124,8 +166,8 @@ export default {
   flex-direction: column;
   display: flex;
 
-  h5{
-      color:var(--humber-light);
+  h5 {
+    color: var(--humber-light);
   }
 }
 
@@ -133,18 +175,23 @@ export default {
   background: var(--humber-dark);
 }
 .card {
-    position: relative;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    min-width: 0;
-    word-wrap: break-word;
-    background-color: #ffffff42;
-    background-clip: border-box;
-    border: 1px solid rgba(0,0,0,.125);
-    border-radius: 0.25rem
+  position: relative;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #ffffff42;
+  background-clip: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+}
 
-;
+@media (max-width: 749px) {
+  .card {
+    width: 100% !important;
+    // margin-left: 12px;
+  }
 }
 </style>
---
+++ src/pages/academy/pricingTable.vue
@@ -0,0 +1,145 @@
+<template>
+  <section class="pricing py-5">
+  <div class="container-fluid">
+    <div class="row">
+      <!-- Free Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-light text-uppercase text-center">Module 1</h5>
+            
+            <hr>
+         
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fa fa-check"></i></span> Analog & Digital Audio</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Introduction to FL Studio</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Working with MIDI & Samples</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Production Analysis</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Drum Samples & Programming</li>
+             
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Sign up</a>
+          </div>
+        </div>
+      </div>
+      <!-- Plus Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-light text-uppercase text-center">Module 2</h5>
+            
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fa fa-check"></i></span><strong>Music Theory</strong></li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Programming Basslines</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Working with Synths & Instruments</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Structure & Arrangement</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Programming Melody Lines</li>
+             
+             
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Sign up</a>
+          </div>
+        </div>
+
+
+      </div>
+      <!-- Pro Tier -->
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-light text-uppercase text-center">Module 3</h5>
+         
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fa fa-check"></i></span><strong>Working with the Mixer</strong></li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Effects, Equalization & Dynamics</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Mixing Techniques</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Introduction to Mastering</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Final Project</li>
+            
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Sign up</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    section.pricing {
+  /* background: #007bff;
+  background: linear-gradient(to right, #0062E6, #33AEFF); */
+}
+
+.pricing .card {
+  border: none;
+  border-radius: 1rem;
+  transition: all 0.2s;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+  background:#212121;
+}
+li{
+    color:white
+}
+.pricing hr {
+  margin: 1.5rem 0;
+}
+
+.pricing .card-title {
+  margin: 0.5rem 0;
+  font-size: 0.9rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+}
+
+.pricing .card-price {
+  font-size: 3rem;
+  margin: 0;
+}
+
+.pricing .card-price .period {
+  font-size: 0.8rem;
+}
+
+.pricing ul li {
+  margin-bottom: 1rem;
+}
+
+.pricing .text-muted {
+  opacity: 0.7;
+}
+
+.pricing .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  opacity: 0.7;
+  transition: all 0.2s;
+  background:#000;
+  border:none
+}
+
+/* Hover Effects on Card */
+
+@media (min-width: 992px) {
+  .pricing .card:hover {
+    margin-top: -.25rem;
+    margin-bottom: .25rem;
+    box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.3);
+  }
+  .pricing .card:hover .btn {
+    opacity: 1;
+  }
+}
+</style>
